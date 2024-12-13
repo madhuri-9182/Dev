6108aa9045588511f704577c7cc92bafceb60a64
samarth-kumar-here <samarth@hiringdog.com>
2024-12-13 19:11:06 +0530

    Update Finance.jsx

--- src/Components/Internal/Finance.jsx
+++ src/Components/Internal/Finance.jsx
@@ -136,13 +136,24 @@ const Finance = () => {
           </select>
         </div>
         <button style={styles.ratesButton}>Rates</button>
-        <input
+        {/* <input
           type="text"
           placeholder="Search Users by Name"
           style={styles.searchBar}
         />
-        <img src="https://img.icons8.com/?size=100&id=132&format=png&color=000000" alt="" style={{height:"20px",display:"flex",position:"absolute",right:"100px"}} />
         
+        <img src="https://img.icons8.com/?size=100&id=132&format=png&color=000000" alt="" style={{height:"20px",display:"flex",position:"absolute",right:"100px"}} /> */}
+        <div className="flex flex-col justify-end sm:flex-row sm:items-center sm:space-x-4 space-y-4 sm:space-y-0 ml-auto">
+             
+              <div className="flex items-center bg-gray-100 rounded-full px-4 py-2 w-full sm:w-80">
+                <input
+                  type="text"
+                  placeholder="Search Client by name"
+                  className="flex-1 bg-transparent text-gray-600 outline-none text-sm"
+                />
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" /></svg>
+          </div>
+          </div>
         
 
 
