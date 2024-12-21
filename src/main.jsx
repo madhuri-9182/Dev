2a463b469a44e6f10ecba000b0a7f7023d2e1946
Sumit Kumar Mahto <sumit.kumar.mahto341@gmail.com>
2024-12-21 12:34:18 +0530

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import { SignIn, SignUp, ForgetPass, PasswordReset, SignUpSignInLayout, page, LoginUsingEmail, LoginUsingNumber } from './Components'
 import { ClientAddCandidate } from './Components/Client/InterviewPlatform/VerticalNavigationLinks/AddCandidate'
 import { ClientScheduleInterview } from './Components/Client/InterviewPlatform/VerticalNavigationLinks/ScheduleInterview'
-import AddJob from './Components/Client/InterviewPlatform/VerticalNavigationLinks/AddJOb'
+import AddJob from './Components/Client/InterviewPlatform/VerticalNavigationLinks/AddJob'
 // Client Imports
 import { NavigationLayout, Dashboard, Settings, Jobs, Candidates, Analytics,AnalyticsDateFilter, Integration, Finance, Engagement, Message } from './Components'
 
