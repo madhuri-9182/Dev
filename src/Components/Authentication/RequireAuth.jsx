fbeef7fcf273fad93810ab393a5b92ba1bc0860c
Nishant <nishant@hiringdog.com>
2025-02-27 13:09:11 +0530

    feat: enhance RequireAuth component to redirect users based on roles and access token

--- src/Components/Authentication/RequireAuth.jsx
+++ src/Components/Authentication/RequireAuth.jsx
@@ -5,20 +5,46 @@ import {
 } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import PropTypes from "prop-types";
+import {
+  ROLES,
+  ROLES_REDIRECTS,
+} from "../Constants/constants";
 
 const RequireAuth = ({ allowedRoles }) => {
   const { auth } = useAuth();
   const location = useLocation();
 
-  return allowedRoles.includes(auth?.role) ? (
-    <Outlet />
-  ) : auth?.accessToken ? (
-    <Navigate
-      to="/unauthorized"
-      state={{ from: location }}
-      replace
-    />
-  ) : (
+  const userMainRole = Object.entries(ROLES).find(
+    // eslint-disable-next-line no-unused-vars
+    ([_, roles]) => roles.includes(auth?.role)
+  )?.[0];
+
+  const isRootPath = location.pathname === "/";
+
+  if (allowedRoles.includes(auth?.role)) {
+    return <Outlet />;
+  }
+
+  if (auth?.accessToken) {
+    if (isRootPath) {
+      return (
+        <Navigate
+          to={ROLES_REDIRECTS[userMainRole]}
+          replace
+        />
+      );
+    } else {
+      return (
+        <Navigate
+          to="/unauthorized"
+          state={{ from: location }}
+          replace
+        />
+      );
+    }
+  }
+
+  return (
     <Navigate
       to="/auth/signin/loginmail"
       state={{ from: location }}
