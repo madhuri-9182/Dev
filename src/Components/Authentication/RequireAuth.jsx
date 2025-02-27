af877430e3b1ebaa55e78cead6e0a75b8d560925
Ramesh <ramesh.kumar@scalenut.com>
2025-02-27 23:49:15 +0530

    UI changes in engagement section and role redirect on refresh  on root route

--- src/Components/Authentication/RequireAuth.jsx
+++ src/Components/Authentication/RequireAuth.jsx
@@ -1,29 +1,29 @@
-import {
-  useLocation,
-  Navigate,
-  Outlet,
-} from "react-router-dom";
+import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import PropTypes from "prop-types";
+import { ROLES, ROLES_REDIRECTS } from "../Constants/constants";
 
 const RequireAuth = ({ allowedRoles }) => {
   const { auth } = useAuth();
   const location = useLocation();
 
+  const getUserType = () => {
+    return (
+      Object.entries(ROLES).find(([key, roles]) =>
+        roles.includes(auth?.role)
+      )?.[0] || "UNKNOWN"
+    );
+  };
   return allowedRoles.includes(auth?.role) ? (
     <Outlet />
   ) : auth?.accessToken ? (
     <Navigate
-      to="/unauthorized"
+      to={`${ROLES_REDIRECTS[getUserType()]}`}
       state={{ from: location }}
       replace
     />
   ) : (
-    <Navigate
-      to="/auth/signin/loginmail"
-      state={{ from: location }}
-      replace
-    />
+    <Navigate to="/auth/signin/loginmail" state={{ from: location }} replace />
   );
 };
 
---
+++ src/Components/Client/Engagement/components/StyledSelect.jsx
@@ -0,0 +1,12 @@
+import { MenuItem, Select, styled } from "@mui/material";
+
+export const StyledSelect = styled(Select)({
+  borderRadius: "8px",
+  fontSize: "12px",
+  height: "40px",
+  minWidth: 120,
+});
+
+export const StyledMenuItem = styled(MenuItem)({
+  fontSize: "12px",
+});
