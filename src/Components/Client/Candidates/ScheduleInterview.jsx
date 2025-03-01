b9587d2550bebaa1d427b084e8436654ac3a0eee
Nishant <nishant@hiringdog.com>
2025-03-01 16:15:29 +0530

    feat: move base64ToFile utility function to util.js and import it in ScheduleInterview component

--- src/Components/Client/Candidates/ScheduleInterview.jsx
+++ src/Components/Client/Candidates/ScheduleInterview.jsx
@@ -3,7 +3,10 @@ import useAllJobs from "../../../hooks/useFetchAllJobs";
 import { useLocation } from "react-router-dom";
 import BasicDatePicker from "../../../utils/BasicDatePicker";
 import { useNavigate } from "react-router-dom";
-import { formatExperience } from "../../../utils/util";
+import {
+  base64ToFile,
+  formatExperience,
+} from "../../../utils/util";
 import {
   CANDIDATE_SOURCE,
   JOB_NAMES,
@@ -44,20 +47,6 @@ function ClientScheduleInterview() {
 
   const item = encodedData ? JSON.parse(encodedData) : null;
   const [remark, setRemark] = useState(item.remark ?? "");
-
-  const base64ToFile = (base64, filename) => {
-    const arr = base64.split(",");
-    const mime = arr[0].match(/:(.*?);/)[1];
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-
-    return new File([u8arr], filename, { type: mime });
-  };
 
   useEffect(() => {
     if (item?.fileBase64) {
--- src/utils/util.js
+++ src/utils/util.js
@@ -59,6 +59,20 @@ export const fileToBase64 = (file) => {
   });
 };
 
+export const base64ToFile = (base64, filename) => {
+  const arr = base64.split(",");
+  const mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return new File([u8arr], filename, { type: mime });
+};
+
 export const createFileFromUrl = async (url) => {
   const response = await axios.get(url, {
     responseType: "blob",
