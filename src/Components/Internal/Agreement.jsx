ce2598e12de17300942cd7954471d0325de388c4
Amanshu Taklikar <taklikaramanshu@gmail.com>
2025-03-08 21:46:33 +0530

    Agreements UI improvement

--- src/Components/Internal/Agreement.jsx
+++ src/Components/Internal/Agreement.jsx
@@ -183,111 +183,110 @@ function Agreement() {
               }`}
           >
             {data.slice(0, showAll ? data.length : data.length).map((item, index) => (
-              <div className='font-semibold  ' >
-              <div
-                key={index}
-                className={`${editClientUser === index ? "bg-none border border-black" : "bg-[#EBEBEB]"} grid grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr_0.5fr] mx-5 mt-2 rounded-[16px] items-center bg-[#EBEBEB80] justify-center max-h-max`}
-              >
-                <div className="px-5 py-1 w-auto ">
-                  <input
-                    type="text"
-                    disabled
-                    value={item.company}
-                    className="block w-full text-left font-bold text-[16px] px-[5px] py-2 text-[#056DDC]"
-                  />
-                </div>
-                <div className="px-3 py-1 w-auto text-black font-semibold  ">
-                  <label htmlFor="">0-4 Years</label>
-                  <input
-                    type="number"
-                    disabled={editClientUser !== index}
-                    value={item.salaries['0-4 Years'].toFixed(2)}
-                    className={`block w-full text-left font-normal px-[5px] py-2 ${editClientUser === index
-                      ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg'
-                      : 'border border-none'
-                      }`}
-                  />
-                </div>
-                <div className="px-3 py-1 w-auto text-black font-semibold  ">
-                  <label htmlFor="">4-6 Years</label>
-                  <input
-                    type="number"
-                    disabled={editClientUser !== index}
-                    value={item.salaries['4-6 Years'].toFixed(2)}
-                    className={`block w-full text-left font-normal px-[5px] py-2 ${editClientUser === index
-                      ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg'
-                      : 'border border-none'
-                      }`}
-                  />
-                </div>
-                <div className="px-3 py-1 w-auto text-black font-semibold  ">
-                  <label htmlFor="">6-8 Years</label>
-                  <input
-                    type="number"
-                    disabled={editClientUser !== index}
-                    value={item.salaries['6-8 Years'].toFixed(2)}
-                    className={`block w-full text-left font-normal px-[5px] py-2 ${editClientUser === index
-                      ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg'
-                      : 'border border-none'
-                      }`}
-                  />
-                </div>
-                <div className="px-3 py-1 w-auto text-black font-semibold  ">
-                  <label htmlFor="">8-10 Years</label>
-                  <input
-                    type="number"
-                    disabled={editClientUser !== index}
-                    value={item.salaries['8-10 Years'].toFixed(2)}
-                    className={`block w-full text-left font-normal px-[5px] py-2 ${editClientUser === index
-                      ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg'
-                      : 'border border-none'
-                      }`}
-                  />
-                </div>
-                <div className="px-3 py-1 w-auto text-black font-semibold  ">
-                  <label htmlFor="">10+ Years</label>
-                  <input
-                    type="number"
-                    disabled={editClientUser !== index}
-                    value={item.salaries['10+ Years'].toFixed(2)}
-                    className={`block w-full text-left font-normal px-[5px] py-2 ${editClientUser === index
-                      ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg'
-                      : 'border border-none'
-                      }`}
-                  />
-                </div>
-                
-                <div className="px-4 py-1 w-full flex items-center justify-center">
-                  {editClientUser === index ?
-                    <div
-                      className='flex items-center justify-center gap-x-2'
-                    >
-                      <button
-                        className='py-1 px-2 rounded-lg font-bold bg-[#056DDC] text-white'
-                        onClick={() => { toggleSaveClientUser(index) }}
-                      >Save</button>
-                      <button
-                        onClick={() => { cancelEdit(index) }}
+              <div key={index} className='font-semibold'>
+                <div
+                  className={`${editClientUser === index ? "bg-none border border-black" : "bg-[#EBEBEB]"} grid grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr_0.5fr] mx-5 mt-2 rounded-[16px] items-center bg-[#EBEBEB80] justify-center max-h-max`}
+                >
+                  <div className="px-5 py-1 w-auto ">
+                    <input
+                      type="text"
+                      disabled
+                      value={item.company}
+                      className="block w-full text-left font-bold text-[16px] px-[5px] py-2 text-[#056DDC]"
+                    />
+                  </div>
+                  <div className="px-3 py-[10px] w-auto text-black font-semibold  ">
+                    <label htmlFor="">0-4 Years</label>
+                    <input
+                      type="number"
+                      disabled={editClientUser !== index}
+                      value={item.salaries['0-4 Years'].toFixed(2)}
+                      className={`block w-full text-left font-normal pt-[4px] ${editClientUser === index
+                        ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg h-[30px] pl-[10px]'
+                        : 'border border-none'
+                        }`}
+                    />
+                  </div>
+                  <div className="px-3 py-[10px] w-auto text-black font-semibold  ">
+                    <label htmlFor="">4-6 Years</label>
+                    <input
+                      type="number"
+                      disabled={editClientUser !== index}
+                      value={item.salaries['4-6 Years'].toFixed(2)}
+                      className={`block w-full text-left font-normal pt-[4px] ${editClientUser === index
+                        ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg h-[30px] pl-[10px]'
+                        : 'border border-none'
+                        }`}
+                    />
+                  </div>
+                  <div className="px-3 py-[10px] w-auto text-black font-semibold  ">
+                    <label htmlFor="">6-8 Years</label>
+                    <input
+                      type="number"
+                      disabled={editClientUser !== index}
+                      value={item.salaries['6-8 Years'].toFixed(2)}
+                      className={`block w-full text-left font-normal pt-[4px] ${editClientUser === index
+                        ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg h-[30px] pl-[10px]'
+                        : 'border border-none'
+                        }`}
+                    />
+                  </div>
+                  <div className="px-3 py-[10px] w-auto text-black font-semibold  ">
+                    <label htmlFor="">8-10 Years</label>
+                    <input
+                      type="number"
+                      disabled={editClientUser !== index}
+                      value={item.salaries['8-10 Years'].toFixed(2)}
+                      className={`block w-full text-left font-normal pt-[4px] ${editClientUser === index
+                        ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg h-[30px] pl-[10px]'
+                        : 'border border-none'
+                        }`}
+                    />
+                  </div>
+                  <div className="px-3 py-[10px] w-auto text-black font-semibold  ">
+                    <label htmlFor="">10+ Years</label>
+                    <input
+                      type="number"
+                      disabled={editClientUser !== index}
+                      value={item.salaries['10+ Years'].toFixed(2)}
+                      className={`block w-full text-left font-normal pt-[4px] ${editClientUser === index
+                        ? ' focus:outline-none border border-[#E8DEF8] focus:ring-1 focus:ring-blue-500 rounded-lg h-[30px] pl-[10px]'
+                        : 'border border-none'
+                        }`}
+                    />
+                  </div>
+                  
+                  <div className="px-4 py-1 w-full flex items-center justify-center">
+                    {editClientUser === index ?
+                      <div
+                        className='flex items-center justify-center gap-x-2'
                       >
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" /></svg>
+                        <button
+                          className='py-1 px-2 rounded-lg font-bold bg-[#056DDC] text-white'
+                          onClick={() => { toggleSaveClientUser(index) }}
+                        >Save</button>
+                        <button
+                          onClick={() => { cancelEdit(index) }}
+                        >
+                          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" /></svg>
+                        </button>
+
+
+                      </div>
+
+                      :
+                      <button
+                        className="p-1 bg-gray-200 shadow-md hover:bg-gray-300 rounded-lg"
+                        onClick={() => { toggleEditClientUser(index) }}
+                      >
+                        <svg width="24" height="24" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M7 21H8.425L18.2 11.225L16.775 9.8L7 19.575V21ZM5 23V18.75L18.2 5.575C18.4 5.39167 18.6208 5.25 18.8625 5.15C19.1042 5.05 19.3583 5 19.625 5C19.8917 5 20.15 5.05 20.4 5.15C20.65 5.25 20.8667 5.4 21.05 5.6L22.425 7C22.625 7.18333 22.7708 7.4 22.8625 7.65C22.9542 7.9 23 8.15 23 8.4C23 8.66667 22.9542 8.92083 22.8625 9.1625C22.7708 9.40417 22.625 9.625 22.425 9.825L9.25 23H5ZM17.475 10.525L16.775 9.8L18.2 11.225L17.475 10.525Z" fill="#65558F" />
+                        </svg>
+
                       </button>
-
-
-                    </div>
-
-                    :
-                    <button
-                      className="p-1 bg-gray-200 shadow-md hover:bg-gray-300 rounded-lg"
-                      onClick={() => { toggleEditClientUser(index) }}
-                    >
-                      <svg width="24" height="24" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7 21H8.425L18.2 11.225L16.775 9.8L7 19.575V21ZM5 23V18.75L18.2 5.575C18.4 5.39167 18.6208 5.25 18.8625 5.15C19.1042 5.05 19.3583 5 19.625 5C19.8917 5 20.15 5.05 20.4 5.15C20.65 5.25 20.8667 5.4 21.05 5.6L22.425 7C22.625 7.18333 22.7708 7.4 22.8625 7.65C22.9542 7.9 23 8.15 23 8.4C23 8.66667 22.9542 8.92083 22.8625 9.1625C22.7708 9.40417 22.625 9.625 22.425 9.825L9.25 23H5ZM17.475 10.525L16.775 9.8L18.2 11.225L17.475 10.525Z" fill="#65558F" />
-                      </svg>
-
-                    </button>
-                  }
+                    }
+                  </div>
                 </div>
-              </div>
               </div>
             ))}
           </div>
