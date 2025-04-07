bd4316fa4076bdfd405a7d83b0b1cecd6f804e63
Nishant <nishant@hiringdog.com>
2025-04-07 13:43:42 +0530

    refactor(Sidebar): comment out unused Link import and related code for clarity

--- src/Components/Interviewer/Dashboard/Components/Sidebar.jsx
+++ src/Components/Interviewer/Dashboard/Components/Sidebar.jsx
@@ -1,6 +1,6 @@
 // src/components/interviewer/components/Sidebar.jsx
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 /**
  * Sidebar component with navigation buttons
@@ -8,13 +8,13 @@ import { Link } from "react-router-dom";
 const Sidebar = ({ handleCalendarClick, isLoading }) => (
   <div className="flex items-start justify-center w-52 py-5">
     <div className="mt-20 py-5 px-3 flex flex-col items-center justify-start rounded-2xl bg-[#E7E4E8CC] h-4/6 gap-y-4">
-      <Link
+      {/* <Link
         to="/interviewer/payments"
         className="py-2 px-3 w-full text-default font-medium border border-[#79747E] rounded-full text-[#65558F] transition-all duration-300 ease-in-out 
           hover:bg-gradient-to-r hover:from-[#ECE8F2] hover:to-[#DCD6E6]"
       >
         Check Receivables
-      </Link>
+      </Link> */}
       <button
         onClick={handleCalendarClick}
         className="py-2 px-4 rounded-full text-default font-medium text-white
