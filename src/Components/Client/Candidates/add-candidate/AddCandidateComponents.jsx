8f5cebdf6ee85f646930b8f03061828c53ee12da
Nishant <nishant@hiringdog.com>
2025-04-26 19:59:39 +0530

    fix: correct experience property references in ResumeTableRow component

--- src/Components/Client/Candidates/add-candidate/AddCandidateComponents.jsx
+++ src/Components/Client/Candidates/add-candidate/AddCandidateComponents.jsx
@@ -9,6 +9,7 @@ import {
 } from "iconsax-react";
 import {
   fileToBase64,
+  formatExperience,
 } from "../../../../utils/util";
 import {
   EMAIL_REGEX,
@@ -128,8 +129,8 @@ const ResumeTableRow = ({
     defaultValues: {
       name: item.name,
       years_of_experience: {
-        year: item.years_of_experience.years,
-        month: item.years_of_experience.months,
+        year: item.years_of_experience.year,
+        month: item.years_of_experience.month,
       },
       phone_number: item.phone_number.startsWith("+91")
         ? item.phone_number.slice(3)
@@ -150,8 +151,8 @@ const ResumeTableRow = ({
     reset({
       name: item.name,
       years_of_experience: {
-        year: item.years_of_experience.years,
-        month: item.years_of_experience.months,
+        year: item.years_of_experience.year,
+        month: item.years_of_experience.month,
       },
       phone_number: item.phone_number.startsWith("+91")
         ? item.phone_number.slice(3)
@@ -356,19 +357,6 @@ const ResumeTableRow = ({
     return item[key] || "-";
   };
 
-  const formatExperience = (experience) => {
-    const { years, months } = experience;
-  
-    if (years === 0 && months === 0) {
-      return "-";
-    }
-  
-    const year = years ? `${years} Years` : "";
-    const month = months ? `${months} Months` : "";
-  
-    return `${year} ${month}`.trim();
-  };
-
   window.removeCandidateFromData = (key) => {
     const updatedKey = key.split("candidateData-")[1];
     const updatedData = data.filter((row) => {
@@ -565,8 +553,8 @@ const ResumeTableRow = ({
                   reset({
                     name: item.name,
                     years_of_experience: {
-                      year: item.years_of_experience.years,
-                      month: item.years_of_experience.months,
+                      year: item.years_of_experience.year,
+                      month: item.years_of_experience.month,
                     },
                     phone_number:
                       item.phone_number.startsWith("+91")
